0d8b9bd2f72c8f1b7347b87785adb786ea45dc6f
Gabriel Chaves Martins <chavesmartinsg@gmail.com>
2022-10-09 22:39:01 -0300

    Starting Service Manager Thread

--- TcpClientServiceManager.cpp
+++ TcpClientServiceManager.cpp
@@ -1,9 +1,16 @@
 #include "TcpClientServiceManager.hpp"
 
 
+void * tcp_client_svc_manager_thread_fn(void *arg){
+    TCPClientServiceManager *svc_manager = (TCPClientServiceManager *)arg;
+    svc_manager->StartTcpClientServiceManagerThreadInternal();
+    return NULL;
+}
 
 const void TCPClientServiceManager::StartTcpClientServiceManagerThread(){
-    
+    pthread_attr_t attr;
+    pthread_attr_init (&attr);
+    pthread_create(client_svc_mgr_thread, &attr, tcp_client_svc_manager_thread_fn, (void*)this);
 }
 
 const void TCPClientServiceManager::ClientFDStartListen(TCPClient *tcp_client){
--- TcpClientServiceManager.hpp
+++ TcpClientServiceManager.hpp
@@ -9,13 +9,25 @@ class TCPServerController;
 
 class TCPClientServiceManager{
 
+	int max_fd{};
+	fd_set active_fd_set;
+	fd_set backup_fd_set;
+
+	pthread_t *client_svc_mgr_thread;
+
+
 	TCPServerController *tcp_ctrl;
 	std::vector<TCPClient *>tcp_client_db;
 
 	public:
-		TCPClientServiceManager(TCPServerController *tcp_ctrl): tcp_ctrl(tcp_ctrl){};
+		TCPClientServiceManager(TCPServerController *tcp_ctrl): tcp_ctrl(tcp_ctrl){
+			FD_ZERO(&active_fd_set);
+			FD_ZERO(&backup_fd_set);
+			client_svc_mgr_thread = (pthread_t *)calloc(1, sizeof(pthread_t));
+		};
 
 		const void StartTcpClientServiceManagerThread();
+		const void StartTcpClientServiceManagerThreadInternal();
 		const void ClientFDStartListen(TCPClient *tcp_client);
 };
 
