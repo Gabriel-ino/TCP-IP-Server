4d7d9a2988ebf571b2b0a55d4b2815784da47832
Gabriel Chaves Martins <chavesmartinsg@gmail.com>
2022-10-10 13:09:46 -0300

    Adding new clients & FIX: Stack Overflow Error

--- TcpClientServiceManager.cpp
+++ TcpClientServiceManager.cpp
@@ -17,21 +17,23 @@ const void TCPClientServiceManager::StartTcpClientServiceManagerThreadInternal()
     TCPClient *tcp_client, *next_tcp_client;
 
     struct sockaddr_in client_addr;
+    std::vector<TCPClient *>::iterator it;
 
     socklen_t addr_len = sizeof(client_addr);
 
     this->max_fd = this->GetMaxFd();
 
-    FD_ZERO(&backup_fd_set);
+    FD_ZERO(&this->backup_fd_set);
 
-    CopyClientFDToFDSet(&backup_fd_set);
+    CopyClientFDToFDSet(&this->backup_fd_set);
 
     while (1){
         memcpy(&this->active_fd_set, &this->backup_fd_set, sizeof(fd_set));
         select(this->max_fd+1, &this->active_fd_set, 0, 0, 0);
 
-        for (auto &i:tcp_client_db){
-            next_tcp_client = ++i;
+        for (it = this->tcp_client_db.begin(), tcp_client = *it; it != this->tcp_client_db.end(); tcp_client = next_tcp_client){
+            
+            next_tcp_client = *(++it);     
 
             if(FD_ISSET(tcp_client->comm_fd, &this->active_fd_set)){
                 recv_bytes = recvfrom(tcp_client->comm_fd,
@@ -47,7 +49,7 @@ const void TCPClientServiceManager::StartTcpClientServiceManagerThreadInternal()
                 }
 
             }
-            tcp_client = next_tcp_client;
+            
         }
 
     }
@@ -57,10 +59,15 @@ const void TCPClientServiceManager::StartTcpClientServiceManagerThreadInternal()
 const void TCPClientServiceManager::StartTcpClientServiceManagerThread(){
     pthread_attr_t attr;
     pthread_attr_init (&attr);
-    pthread_create(client_svc_mgr_thread, &attr, tcp_client_svc_manager_thread_fn, (void*)this);
+    pthread_create(this->client_svc_mgr_thread, &attr, tcp_client_svc_manager_thread_fn, (void*)this);
 }
 
 const void TCPClientServiceManager::ClientFDStartListen(TCPClient *tcp_client){
+    this->StopTcpClientServiceManagerThread();
+    this->AddClientToDB(tcp_client);
+
+    this->client_svc_mgr_thread = (pthread_t *)calloc(1, sizeof(pthread_t));
+    this->StartTcpClientServiceManagerThread();
     
 }
 
@@ -79,15 +86,20 @@ const int TCPClientServiceManager::GetMaxFd(){
 }
 
 const void TCPClientServiceManager::CopyClientFDToFDSet(fd_set *fdset) {
-		for (auto &i:tcp_client_db){
+		for (auto &i:this->tcp_client_db){
         	FD_SET(i->comm_fd, fdset);
     	}	
 	}
 
 
 const void TCPClientServiceManager::StopTcpClientServiceManagerThread(){
-    pthread_cancel(*client_svc_mgr_thread);
-    pthread_join(*client_svc_mgr_thread, NULL);
-    free(client_svc_mgr_thread);
-    client_svc_mgr_thread = NULL;
+    pthread_cancel(*this->client_svc_mgr_thread);
+    pthread_join(*this->client_svc_mgr_thread, NULL);
+    free(this->client_svc_mgr_thread);
+    this->client_svc_mgr_thread = NULL;
+}
+
+
+const void TCPClientServiceManager::AddClientToDB(TCPClient *client){
+    this->tcp_client_db.push_back(client);
 }
--- TcpClientServiceManager.hpp
+++ TcpClientServiceManager.hpp
@@ -20,6 +20,7 @@ class TCPClientServiceManager{
 	int max_fd{};
 	fd_set active_fd_set;
 	fd_set backup_fd_set;
+	bool hasOnlyOneClient{true};
 
 	pthread_t *client_svc_mgr_thread;
 
@@ -35,13 +36,14 @@ class TCPClientServiceManager{
 		TCPClientServiceManager(TCPServerController *tcp_ctrl): tcp_ctrl(tcp_ctrl){
 			FD_ZERO(&active_fd_set);
 			FD_ZERO(&backup_fd_set);
-			client_svc_mgr_thread = (pthread_t *)calloc(1, sizeof(pthread_t));
+			this->client_svc_mgr_thread = (pthread_t *)calloc(1, sizeof(pthread_t));
 		};
 
 		const void StartTcpClientServiceManagerThread();
 		const void StopTcpClientServiceManagerThread();
 		const void StartTcpClientServiceManagerThreadInternal();
 		const void ClientFDStartListen(TCPClient *tcp_client);
+		const void AddClientToDB(TCPClient *);
 };
 
 
